1178530abb14de1ef4b420d88bae29abbb686afe
Galyminsky-PC <jobforandroid@protonmail.com>
2022-10-31 12:13:03 +0600

    Add ---> permission MainViewModel

--- app/src/main/java/com/protonmail/jobforandroid/weather/MainActivity.kt
+++ app/src/main/java/com/protonmail/jobforandroid/weather/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import com.protonmail.jobforandroid.weather.fragments.MainFragment
 
 const val API_KEY = "543cbe8f60c8411f9f6133943222710"
+//www.weatherapi.com ---> API
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
---
+++ app/src/main/java/com/protonmail/jobforandroid/weather/MainViewModel.kt
@@ -0,0 +1,9 @@
+package com.protonmail.jobforandroid.weather
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    val liveDataCurrent = MutableLiveData<String>()
+    val liveDataList = MutableLiveData<String>()
+}
