830ebfaeca95af4347b29c1b03c5813d7b374532
Galyminsky-PC <jobforandroid@protonmail.com>
2022-11-01 13:25:55 +0600

    RecyclerView in HoursFragment

--- app/src/main/java/com/protonmail/jobforandroid/weather/adapters/WeatherAdapter.kt
+++ app/src/main/java/com/protonmail/jobforandroid/weather/adapters/WeatherAdapter.kt
@@ -1,11 +1,15 @@
 package com.protonmail.jobforandroid.weather.adapters
 
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.protonmail.jobforandroid.weather.R
 import com.protonmail.jobforandroid.weather.databinding.ListItemBinding
 
-class WeatherAdapter : ListAdapter<WeatherModel, WeatherAdapter.Holder>() {
+class WeatherAdapter : ListAdapter<WeatherModel, WeatherAdapter.Holder>(Comparator()) {
 
     class Holder(view: View) : RecyclerView.ViewHolder(view) {
         val binding = ListItemBinding.bind(view)
@@ -15,7 +19,25 @@ class WeatherAdapter : ListAdapter<WeatherModel, WeatherAdapter.Holder>() {
             tvCondition.text = item.condition
             tvTemp.text = item.currentTemp
         }
+    }
 
+    class Comparator : DiffUtil.ItemCallback<WeatherModel>() {
+        override fun areItemsTheSame(oldItem: WeatherModel, newItem: WeatherModel): Boolean {
+            return oldItem == newItem
+        }
+
+        override fun areContentsTheSame(oldItem: WeatherModel, newItem: WeatherModel): Boolean {
+            return oldItem == newItem
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.list_item, parent, false)
+        return Holder(view)
+    }
+
+    override fun onBindViewHolder(holder: Holder, position: Int) {
+        holder.bind(getItem(position))
     }
 
 }
