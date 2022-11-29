85a403d3f532a4bab43b84b40d794f9ee22eb6dc
Galyaminsky <jobforandroid@protonmail.com>
2022-11-29 08:39:09 +0600

    _Binding API_KEY #2

--- app/src/main/java/com/protonmail/jobforandroid/weather/MainActivity.kt
+++ app/src/main/java/com/protonmail/jobforandroid/weather/MainActivity.kt
@@ -2,15 +2,18 @@ package com.protonmail.jobforandroid.weather
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.protonmail.jobforandroid.weather.databinding.ActivityMainBinding
 import com.protonmail.jobforandroid.weather.fragments.MainFragment
 
 
-//www.weatherapi.com ---> API
-
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
 
         supportFragmentManager
             .beginTransaction()
